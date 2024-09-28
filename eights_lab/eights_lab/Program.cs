5f326b45ee1706dd7a3afc782386c250fe6094d7
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-28 21:04:13 +0300

    added eigths lab

---
+++ eights_lab/eights_lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace eights_lab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть текст для перевірки:");
+        string inputText = Console.ReadLine();
+
+        string ukrainianPattern = @"^[А-ЯІЇЄҐа-яіїєґ\s]+$";
+
+        Regex regex = new Regex(ukrainianPattern);
+
+        if (regex.IsMatch(inputText))
+        {
+            Console.WriteLine("Український текст.");
+        }
+        else
+        {
+            Console.WriteLine("Не український текст.");
+        }
+    }
+}
+
